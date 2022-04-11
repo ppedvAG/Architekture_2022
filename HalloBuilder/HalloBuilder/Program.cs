8fa0d7f5428cc5a61b7e79f6c8a577e2a76e7e33
André <AndreR@outlook.de>
2022-04-11 13:59:17 +0200

    Builder Pattern mit Community Gaurds

---
+++ HalloBuilder/HalloBuilder/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HalloBuilder;
+
+Console.WriteLine("Hello, World!");
+
+var meinSchrank = new Schrank.Builder()
+    .Türen(12)
+    .Oberfläche(Oberfläche.Lackiert)
+    .Build();
---
+++ HalloBuilder/HalloBuilder/Schrank.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Toolkit.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloBuilder
+{
+    internal class Schrank
+    {
+        private Schrank()
+        {
+        }
+
+        public int AnzTüren { get; private set; }
+        public int AnzBöden { get; private set; }
+        public string Farbe { get; private set; }
+        public Oberfläche Oberfläche { get; private set; }
+
+
+        public class Builder
+        {
+            private Schrank _newSchrank = new();
+
+            public Builder Türen(int anzTüren)
+            {
+                if (anzTüren <= 0)
+                    //throw new ArgumentException("Mehr Türen!!!!");
+                    Guard.IsLessThanOrEqualTo(anzTüren, 2, nameof(anzTüren));
+
+                _newSchrank.AnzTüren = anzTüren;
+                return this;
+            }
+
+            public Builder Oberfläche(Oberfläche oberfläche)
+            {
+                _newSchrank.Oberfläche = oberfläche;
+                return this;
+            }
+
+            public Builder Farbe(string farbe)
+            {
+                Guard.IsNullOrWhiteSpace(farbe, nameof(farbe));
+                if (_newSchrank.Oberfläche != HalloBuilder.Oberfläche.Lackiert)
+                    ThrowHelper.ThrowArgumentException(nameof(farbe), "Farbe nur bei Lackierung");
+
+                _newSchrank.Farbe = farbe;
+                return this;
+            }
+
+            public Schrank Build()
+            {
+                return _newSchrank;
+            }
+
+        }
+
+
+    }
+
+    enum Oberfläche
+    {
+        Unbehandelt,
+        Gewachst,
+        Lackiert
+    }
+}
