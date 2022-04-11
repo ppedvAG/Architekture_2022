be332f821d71826557001f2a0c14b87e54eec82e
André <AndreR@outlook.de>
2022-04-11 14:40:25 +0200

    Deko pattern

---
+++ HalloDekorator/HalloDekorator/Deko.cs
@@ -0,0 +1,65 @@
+﻿namespace HalloDekorator
+{
+    internal interface IComponent
+    {
+        public string Beschreibung { get; }
+        public decimal Preis { get; }
+    }
+
+    abstract class Deko : IComponent
+    {
+        protected readonly IComponent parent;
+
+        public Deko(IComponent parent)
+        {
+            this.parent = parent;
+        }
+
+        public abstract string Beschreibung { get; }
+        public abstract decimal Preis { get; }
+    }
+
+    class Ei : Deko
+    {
+        public Ei(IComponent parent) : base(parent)
+        { }
+
+        public override string Beschreibung => parent.Beschreibung + " Ei";
+
+        public override decimal Preis => parent.Preis + 0.27m;
+    }
+
+    class Käse : Deko
+    {
+        public Käse(IComponent parent) : base(parent)
+        { }
+
+        public override string Beschreibung => parent.Beschreibung + " Käse";
+
+        public override decimal Preis => parent.Preis + 2.30m;
+    }
+
+    class Salami : Deko
+    {
+        public Salami(IComponent parent) : base(parent)
+        { }
+
+        public override string Beschreibung => parent.Beschreibung + " Salami";
+
+        public override decimal Preis => parent.Preis + 2m;
+    }
+
+    class Pizza : IComponent
+    {
+        public string Beschreibung => "Pizza";
+
+        public decimal Preis => 6m;
+    }
+
+    class Brot : IComponent
+    {
+        public string Beschreibung => "Brot";
+
+        public decimal Preis => 2m;
+    }
+}
---
+++ HalloDekorator/HalloDekorator/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HalloDekorator;
+
+var meinePizza = new Käse(new Käse(new Pizza()));
+Console.WriteLine($"Pizza: {meinePizza.Beschreibung} {meinePizza.Preis:c}");
+
+var frühstück = new Salami(new Ei(new Brot()));
+Console.WriteLine($"Brot: {frühstück.Beschreibung} {frühstück.Preis:c}");
+
+
+
