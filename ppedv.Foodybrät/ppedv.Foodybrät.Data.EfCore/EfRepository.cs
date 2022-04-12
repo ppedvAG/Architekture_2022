95249fd8bdf99a087c6479bb676c104531b77550
André <AndreR@outlook.de>
2022-04-12 14:15:54 +0200

    Basic Repo eingebaut

---
+++ ppedv.Foodybrät/ppedv.Foodybrät.Data.EfCore/EfRepository.cs
@@ -0,0 +1,47 @@
+﻿using ppedv.Foodybrät.Contracts;
+using ppedv.Foodybrät.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ppedv.Foodybrät.Data.EfCore
+{
+    public class EfRepository : IRepository
+    {
+
+        EfContext _efContext = new EfContext();
+
+        public void Add<T>(T entity) where T : Entity
+        {
+            //if (typeof(T) == typeof(Food))
+            //    _efContext.Food.Add(entity as Food);
+
+            _efContext.Set<T>().Add(entity);
+            //_efContext.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : Entity
+        {
+            _efContext.Set<T>().Remove(entity);
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : Entity
+        {
+            return _efContext.Set<T>().ToList();
+        }
+
+        public T GetById<T>(int id) where T : Entity
+        {
+            return _efContext.Set<T>().Find(id);
+        }
+
+        public void SaveAll()
+        {
+            _efContext.SaveChanges();
+        }
+
+        public void Update<T>(T entity) where T : Entity
+        {
+            _efContext.Set<T>().Update(entity);
+        }
+    }
+}
---
+++ ppedv.Foodybrät/ppedv.Foodybrät.Logic/Core.cs
@@ -0,0 +1,31 @@
+﻿using ppedv.Foodybrät.Contracts;
+using ppedv.Foodybrät.Model;
+
+namespace ppedv.Foodybrät.Logic
+{
+    public class Core
+    {
+        public IRepository Repository { get; init; }
+
+        public Core() : this(new Data.EfCore.EfRepository())
+        { }
+
+        public Core(IRepository repository)
+        {
+            Repository = repository;
+        }
+
+        public Customer? GetCustomersWithMostyValuableOrders()
+        {
+            return Repository.GetAll<Customer>()
+                             .OrderBy(x => x.Orders.Select(y => y.Items)
+                                                   .Sum(y => y.Sum(z => z.Amount * z.Food.Price)))
+                             .FirstOrDefault();
+        }
+
+        public bool IsOrderVegan(Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
