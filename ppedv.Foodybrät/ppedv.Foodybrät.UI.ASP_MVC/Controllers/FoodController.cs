cb3cd7294b9ee9161b3968f6e5a2826597d0dd36
André <AndreR@outlook.de>
2022-04-13 14:53:29 +0200

    ASP MVC

---
+++ ppedv.Foodybrät/ppedv.Foodybrät.UI.ASP_MVC/Controllers/FoodController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ppedv.Foodybrät.Contracts;
+using ppedv.Foodybrät.Logic;
+using ppedv.Foodybrät.Model;
+
+namespace ppedv.Foodybrät.UI.ASP_MVC.Controllers
+{
+    public class FoodController : Controller
+    {
+        Core core;
+        public FoodController(IUnitOfWork uow = null)
+        {
+            core = new Core(uow);
+        }
+
+        // GET: FoodController
+        public ActionResult Index()
+        {
+            return View(core.UnitOfWork.FoodRepository.Query());
+            return View();
+        }
+
+        // GET: FoodController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: FoodController/Create
+        public ActionResult Create()
+        {
+            return View(new Food() { Description = "NEU" });
+        }
+
+        // POST: FoodController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Food newFood)
+        {
+            try
+            {
+
+                core.UnitOfWork.FoodRepository.Add(newFood);
+                core.UnitOfWork.SaveAll();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FoodController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: FoodController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Food food)
+        {
+            try
+            {
+                core.UnitOfWork.FoodRepository.Update(food);
+                core.UnitOfWork.SaveAll();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FoodController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(core.UnitOfWork.FoodRepository.GetById(id));
+        }
+
+        // POST: FoodController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                var toKill = core.UnitOfWork.FoodRepository.GetById(id);
+                core.UnitOfWork.FoodRepository.Delete(toKill);
+                core.UnitOfWork.SaveAll();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ppedv.Foodybrät/ppedv.Foodybrät.UI.ASP_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ppedv.Foodybrät.UI.ASP_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
